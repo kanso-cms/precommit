ef2245ca7487b9e720232afbce52cd31e0828ad8
Joe Howard <joe.howard.au@icloud.com>
2018-11-15 19:32:01 +1100

    Initital Commit

---
+++ bin/sami.php
@@ -0,0 +1,39 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$dir = dirname(dirname(__FILE__));
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('api')
+    ->exclude('bin')
+    ->exclude('vendor')
+    ->exclude('app')
+    ->exclude('tests')
+    ->exclude('kanso/cms/admin/assets/js/vendor/JavaScriptSpellCheck/core/php')
+    ->in($dir)
+;
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('2.0.*')
+    ->addFromTags('3.0.*')
+    ->add('master', 'master branch')
+;
+
+return new Sami($iterator, array(
+    'theme'                => 'default',
+    'versions'             => $versions,
+    'title'                => 'Kanso CMS API',
+    'build_dir'            => __DIR__ . '/../api/build/%version%',
+    'cache_dir'            => __DIR__ . '/../api/cache/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('kanso-cms/cms', $dir),
+    'default_opened_level' => 2,
+));
+
+
